619862a8d59aed1761169d305d5250f8f9d25988
CubillasCar <ccubillasolano@gmail.com>
2020-10-06 04:24:21 -0500

    add spring boot template

---
+++ src/main/java/pe/isil/ProyectoFinalDae2Application.java
@@ -0,0 +1,13 @@
+package pe.isil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalDae2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalDae2Application.class, args);
+	}
+
+}
